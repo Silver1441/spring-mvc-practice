b1ce10e2933be169220e2a42762eb4e0c5161048
Ivan <Kishkan91@gmail.com>
2019-12-11 12:51:21 +0400

    Migration to spring boot

--- src/main/java/com/kishkan/epam/config/WebConfig.java
+++ src/main/java/com/kishkan/epam/config/WebConfig.java
@@ -1,34 +1,9 @@
 package com.kishkan.epam.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@EnableWebMvc
+
 @Configuration
-@Import({SecurityConfig.class})
-@ComponentScan(basePackages = {
-        "com.kishkan.epam.controller",
-        "com.kishkan.epam.service",
-        "com.kishkan.epam.repository",
-        "com.kishkan.epam.validator"})
-public class WebConfig implements WebMvcConfigurer {
+public class WebConfig {
 
-    @Override
-    public void configureViewResolvers(ViewResolverRegistry registry) {
-        registry.jsp("/WEB-INF/pages/", ".jsp");
-    }
-
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry
-                .addResourceHandler("/resources/**")
-                .addResourceLocations("/resources/");
-    }
 }
